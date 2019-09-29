a1c3be86c20c329e6536b515cdf5645eaef8ef7a
Eric Coffman <ecoffman@tnc.org>
2019-09-29 08:40:49 -0400

    Initial commit.

---
+++ LFDocsToDatabase/DataExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LFDocsToDatabase
+{
+    internal class DataExtractor
+    {
+        private readonly string _doc_path;
+
+        public DataExtractor(string doc_path)
+        {
+            _doc_path = doc_path;
+        }
+    }
+}
---
+++ LFDocsToDatabase/Program.cs
@@ -0,0 +1,54 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LFDocsToDatabase
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+              .WithParsed<Options>(opts => RunOptionsAndReturnExitCode(opts))
+              .WithNotParsed<Options>((errs) => HandleParseError(errs));
+        }
+
+        private static void HandleParseError(IEnumerable<Error> errs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RunOptionsAndReturnExitCode(Options opts)
+        {
+            if (Directory.Exists(opts.DocumentFolderPath))
+            {
+                // This path is a directory
+                ProcessDirectory(opts.DocumentFolderPath);
+            }
+            else
+            {
+                throw new DirectoryNotFoundException(opts.DocumentFolderPath);
+            }
+        }
+
+        public static void ProcessDirectory(string targetDirectory)
+        {
+            // Process the list of files found in the directory.
+            string[] fileEntries = Directory.GetFiles(targetDirectory);
+            foreach (string fileName in fileEntries)
+                ProcessFile(fileName);
+        }
+
+        public static void ProcessFile(string path)
+        {
+            Console.WriteLine("Processed file '{0}'.", path);
+        }
+    }
+
+    internal class Options
+    {
+        [Option('d', "docfolder", Required = true, HelpText = "Path to folder holding documents to parse.")]
+        public string DocumentFolderPath { get; set; }
+    }
+}
